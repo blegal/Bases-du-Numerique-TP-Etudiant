f4b0144007799403784818b5688e7f24c272d058
Bertrand LE GAL <blegal@users.noreply.github.com>
2025-09-03 16:43:11 +0200

    Step 7

---
+++ 7_chenillard_fsm/cvt_50MHz_1Hz.vhd
@@ -0,0 +1,34 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+ENTITY cvt_50MHz_1Hz IS
+	PORT (
+		CLK : in  STD_LOGIC;
+		RST : in  STD_LOGIC;
+		O   : out STD_LOGIC
+	);
+END cvt_50MHz_1Hz;
+
+ARCHITECTURE Behavioral OF cvt_50MHz_1Hz IS
+	CONSTANT MAX_VAL : INTEGER := 24999999;	-- output clock is 1 Hz when input clock is 50 MHz
+	SIGNAL   COUNTER : INTEGER RANGE 0 TO MAX_VAL;
+BEGIN
+
+	PROCESS(RST, CLK)
+	BEGIN
+		IF RST = '1' THEN
+			O       <= '0';
+			COUNTER <= 0;
+		ELSIF CLK = '1' AND CLK'EVENT THEN
+			IF COUNTER = MAX_VAL THEN
+				COUNTER <=  0;
+				O       <= '1';
+			ELSE
+				COUNTER <= COUNTER + 1;
+				O       <= '0';
+			END IF;
+		END IF;
+	END PROCESS;
+
+END Behavioral;
